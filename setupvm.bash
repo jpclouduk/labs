dec60ce940ddd979dfc84db2f8db4cb9df98c535
jpclouduk <poulsenjx@gmail.com>
2023-11-20 20:09:01 +0000

    Update setupvm.bash

--- setupvm.bash
+++ setupvm.bash
@@ -2,7 +2,7 @@
 #
 #
 # Add Docker's official GPG key:
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
